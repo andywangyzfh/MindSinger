cf73c3c242fc7a62edc4cce228e511adabda4f50
Neo Wang <>
2022-03-25 22:21:35 -0400

    spotifyAPI updated.

--- spotifyAPI.py
+++ spotifyAPI.py
@@ -1,14 +1,16 @@
-from os import spawnl
 import spotipy
-import sys
+
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.oauth2 import SpotifyClientCredentials
 
-'''
-Parameter: 
- - q: the string we want to search (e.g. 'sad')
-'''
+
 def search(newq): 
+    '''
+    Parameter
+    - q: the string we want to search (e.g. 'sad')
+    Return 
+    - tuple type, (<name of the playlist>, <image url>)
+    '''
     q = str() 
     q = str(newq)
 
@@ -20,17 +22,26 @@ def search(newq):
     results = sp.search(q='playlist:' + q, type='playlist')
     items = results['playlists']['items']
 
+    blacklist = ['workout', 'gym'] 
+
+    ifContainBlackword = False
+
     for i in range(len(items)): 
         playlist = items[i]
+        name = playlist['name'] 
+        
+        for blackword in blacklist: 
+            # if the name contains blackword, just skip to the next playlist 
+            if (blackword in name ):
+                ifContainBlackword = True 
+                break
+        if (ifContainBlackword == False):
+            return ( (playlist['name']), playlist['images'][0]['url'] )
+            # if managed to print out one -> terminate 
+            break 
 
-        # If the playlist name contains the following word, just ignore it 
-        # blacklist = ['workout', 'gym']
-
-        if ('workout' not in playlist['name']):
-            print(playlist['name'], playlist['images'][0]['url']) 
-
-
-# search('sad')
+# res = search('sad')
+# print(res)
 # search('happy')
 
 
